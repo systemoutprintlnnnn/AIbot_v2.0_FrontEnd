ee56fb29f2ab6429ca964c779c62ec6be5891e5a
小傅哥 <184172133@qq.com>
2023-06-11 12:07:50 +0800

    feat: chagpt-web 第3节：按钮定义与事件实现

--- src/app/components/home.tsx
+++ src/app/components/home.tsx
@@ -11,12 +11,15 @@ import {
 import dynamic from "next/dynamic";
 import {Path} from "@/app/constants";
 
+import {useAppConfig} from "../store/config";
+
 const Chat = dynamic(async () => (await import("./chat/chat")).Chat);
 const Role = dynamic(async () => (await import("./role/role")).Role);
 
 function Screen() {
+    const config = useAppConfig();
     return (
-        <div className={styles.container}>
+        <div className={`${config.tightBorder ? styles["tight-container"] : styles.container}`}>
             {/* 工具菜单 */}
             <SideBar/>
 
--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -3,16 +3,38 @@ import styles from "./sidebar.module.scss";
 import ChatGPTIcon from "../icons/chatgpt.svg";
 import ChatIcon from "../icons/chat.svg";
 import RoleIcon from "../icons/role.svg";
+import MaxIcon from "../icons/max.svg";
+import MinIcon from "../icons/min.svg";
+import ExitIcon from "../icons/exit.svg";
 
 import {useNavigate} from "react-router-dom";
 import {Path} from "@/app/constants";
+import {IconButton} from "@/app/components/button/button";
+import {useAppConfig} from "@/app/store/config";
 
 export function SideBar() {
 
     const navigate = useNavigate();
+    const config = useAppConfig();
 
     return (
         <div className={styles.sidebar}>
+            <div className={styles["action-button"]}>
+                <IconButton icon={<ExitIcon/>} backgroundColor={"#ff4e4e"} onClick={() => {
+                    alert("尚未实现");
+                }}/>
+                <IconButton icon={<MinIcon/>} backgroundColor={"#f3c910"} onClick={() => {
+                    config.update(
+                        (config) => (config.tightBorder = false),
+                    );
+                }}/>
+                <IconButton icon={<MaxIcon/>} backgroundColor={"#04c204"} onClick={() => {
+                    config.update(
+                        (config) => (config.tightBorder = true),
+                    );
+                }}/>
+            </div>
+
             <div className={styles["sidebar-header"]}>
                 <ChatGPTIcon/>
             </div>
