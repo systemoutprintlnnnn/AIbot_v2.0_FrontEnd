6c109841ece55358def083c06af9a77abae32fdc
小傅哥 <184172133@qq.com>
2023-06-18 14:05:24 +0800

    feat: 展示效果优化

--- src/app/components/dialog/dialog-item.tsx
+++ src/app/components/dialog/dialog-item.tsx
@@ -17,11 +17,11 @@ export function DialogItem(props: Props) {
     const date = new Date(dialog.timestamp);
     const timeString = date.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'});
     return (
-        <div className={`${styles.wrapper} ${selected ? styles.selected : ''}`}
-             onClick={() => onClick(dialog)}>
+        <div className={`${styles.wrapper} ${selected ? styles.selected : ''}`} onClick={() => onClick(dialog)}>
             <div className={styles.left}>
                 <Space size={24}>
-                    <Badge count={dialog.count} size={"small"} color={"#fca7a7"}>
+                    {/* Badge 是 React 提供的组件，这里控制只有选中的才展示对话数 */}
+                    <Badge count={props.selected ? dialog.count : 0} size={"small"} color={"#fca7a7"}>
                         <Avatar shape={"square"} src={dialog.avatar} size={40}/>
                     </Badge>
                 </Space>
--- src/app/components/dialog/dialog-list.tsx
+++ src/app/components/dialog/dialog-list.tsx
@@ -1,3 +1,4 @@
+import styles from "./dialog-list.module.scss";
 import {DialogItem} from "@/app/components/dialog/dialog-item";
 import {DialogType} from "@/types/chat";
 import {DialogResizeableSidebar} from "@/app/components/dialog/dialog-resizeable-sidebar";
@@ -12,7 +13,7 @@ export function DialogList() {
 
     // 测试数据
     const dialog01: DialogType = {
-        avatar: 'https://bugstack.cn/images/system/xingqiu.png',
+        avatar: '/role/bugstack.png',
         dialogId: 123,
         read: true,
         subTitle: '写个java冒泡排序?',
@@ -23,13 +24,13 @@ export function DialogList() {
 
     // 测试数据
     const dialog02: DialogType = {
-        avatar: 'https://bugstack.cn/images/system/xingqiu.png/100*100',
+        avatar: '/role/interview.png',
         dialogId: 124,
         read: true,
         subTitle: 'Hello, how are you?',
         timestamp: Date.now(),
         title: '面试官',
-        count: 0
+        count: 5
     };
 
     dialogs.push(dialog01);
@@ -38,15 +39,40 @@ export function DialogList() {
     return (
         // DialogResizeableSidebar 用于调整对话栏的大小
         <DialogResizeableSidebar>
+            {/*头部操作*/}
+            <div className={styles["dialog-head"]}>
+                <div className={styles["dialog-search-box"]}><input type="text" placeholder="搜索"/></div>
+                <div className={styles["dialog-search-add"]} onClick={() => {
+                    alert("创建会话");
+
+                    // 心里咨询
+                    const dialog03: DialogType = {
+                        avatar: '/role/psychological.png',
+                        dialogId: 125,
+                        read: true,
+                        subTitle: '吹灭别人的灯，不能照亮自己',
+                        timestamp: Date.now(),
+                        title: '心里咨询',
+                        count: 100
+                    };
+
+                    dialogs.unshift(dialog03);
+
+                    // 设置选中，这样也会刷新数据
+                    setSelected(dialog03);
+                }}></div>
+            </div>
+            {/*对话列表*/}
             <div>
                 {/*循环遍历数据，当有数据变更时会自动刷新到页面*/}
-                {dialogs.map((dialog, index) =>(
+                {dialogs.map((dialog, index) => (
                     <DialogItem
                         key={dialog.dialogId}
                         dialog={dialog}
                         selected={selected?.dialogId === dialog.dialogId}
                         onClick={() => {
-                            alert("点击对话");
+                            setSelected(dialog)
+                            alert("选中对话" + dialog.title)
                         }}
                     />
                 ))}
